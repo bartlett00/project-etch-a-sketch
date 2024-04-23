ac92e83bd231281475a3f0c6ef28a965daf3751b
Pax <113464089+bartlett00@users.noreply.github.com>
2024-04-23 14:44:06 -0500

    Create basic CSS styling and script for     filling out grid.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const sketchBlock = document.createElement('div');
+
+
+const container = document.querySelector('#sketch-container');
+
+for (let i = 0; i < 256; i++) {
+    const sketchBlock = document.createElement('div');
+    sketchBlock.setAttribute('class', 'block');
+    container.appendChild(sketchBlock);
+}
+
+const blockSelector = document.querySelectorAll('.block');
+
+for (block in blockSelector) {
+
+}
