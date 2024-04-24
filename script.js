a194506c8478979767beab17bbbaf957bf450878
Pax <113464089+bartlett00@users.noreply.github.com>
2024-04-24 10:17:06 -0500

    Add 'sketching' functionality to grid squares.

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-
 const container = document.querySelector('#sketch-container');
-
 for (let i = 0; i < 256; i++) {
     const sketchBlock = document.createElement('div');
     sketchBlock.setAttribute('class', 'block');
     container.appendChild(sketchBlock);
-}
+    sketchBlock.addEventListener('mouseover', () => {
+        sketchBlock.classList.toggle('hovering');
+    });
+};
 
-const blockSelector = document.querySelectorAll('.block');
-
-for (block in blockSelector) {
-
-}
+const gridBtn = document.querySelector('#newGrid');
+gridBtn.addEventListener('click', () => {
+    
+});
