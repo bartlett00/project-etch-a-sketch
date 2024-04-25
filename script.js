6a2289b730adf5ff4cef0db64c39f3a658b36d17
Pax <113464089+bartlett00@users.noreply.github.com>
2024-04-25 10:17:31 -0500

    Complete project.

--- script.js
+++ script.js
@@ -11,54 +11,47 @@ for (let i = 0; i < 256; i++) {
 */
 
 function createGrid(size) {
-for (let i = 0; i < size; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    container.appendChild(row);
+    if (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    const gridContainer = document.createElement('div');
+    gridContainer.setAttribute('class', 'grid-container');
+    container.appendChild(gridContainer);
+
     for (let i = 0; i < size; i++) {
-        const sketchBlock = document.createElement('div');
-        sketchBlock.setAttribute('class', 'block');
-        row.appendChild(sketchBlock);
-        sketchBlock.addEventListener('mouseover', () => {
-            sketchBlock.classList.toggle('hovering');
-        });
-};
-}
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        gridContainer.appendChild(row);
+        
+        for (let i = 0; i < size; i++) {
+            const sketchBlock = document.createElement('div');
+            sketchBlock.setAttribute('class', 'block');
+            row.appendChild(sketchBlock);
+            sketchBlock.addEventListener('mouseover', () => {
+                sketchBlock.classList.toggle('hovering');
+            });
+        };
+    
+    }
 }
 
 createGrid(16);
-/*
-function generateGrid(size) {
-    let pixelSize = size*size;
-    console.log(pixelSize);
-    document.querySelector('body').removeChild('#sketch-container');
-    const block = document.createElement('div');
-    for (let i = 0; i < pixelSize; i++) {
-        block.setAttribute('class', 'block');
-        container.appendChild(block);
-        block.addEventListener('mouseover', () => {
-            block.classList.toggle('hovering');
-        });
-    }
-}
-*/
-
 
 const gridBtn = document.querySelector('#newGrid');
-const popup = document.querySelector('#popup');
 gridBtn.addEventListener('click', () => {
-  popup.style.display = 'flex';
-  gridBtn.classList.add('hidden');
+    let userGrid = prompt('Enter a number', '16');
+    console.log(userGrid);
+    console.log(typeof userGrid);
+    if (isNaN(Number(userGrid))) {
+        alert('Please enter a number.');
+        createGrid(16);
+    } else if (userGrid > 100) {
+        alert('Please enter a number between 1 and 100.');
+        createGrid(16);
+    } else if (userGrid === null) {
+        createGrid(16);
+    } else {
+        createGrid(userGrid);
+    }
 });
-
-const confirmBtn = document.querySelector('#confirm');
-let newGridSize;
-confirmBtn.addEventListener('click', () => {
-    popup.style.display = 'none';
-    gridBtn.classList.remove('hidden');
-    newGridSize = document.querySelector('#newGridSize').value;
-    console.log(newGridSize);
-    createGrid(newGridSize);
-});
-
-
